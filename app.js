25f8c92745b23a83875ba35a061fd7eaa96991e3
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-11-20 16:56:02 +0100

    push

--- app.js
+++ app.js
@@ -2,22 +2,59 @@ import express from "express";
 import http from "http";
 const app = express();
 import { Server } from "socket.io";
-
+import fs from "fs";
 const server = http.Server(app);
 const io = new Server(server);
 const host = "127.0.0.1";
 const port = 8000;
 
 app.use(express.static("public"));
-
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
+// gestion utilisateur.
+const userList = [];
 
 io.on("connection", (socket) => {
+  socket.emit("welcome", { userList: userList, id: socket.id });
+  socket.on("newUser", (data) => {
+    userList.push({
+      pseudo: data.pseudo,
+      id: socket.id,
+    });
+    socket.broadcast.emit("resNewUser", { userList: userList });
+  });
   socket.on("message", (data) => {
-    console.dir(data);
+    /*     {
+      pseudo: pseudo,
+      time: ms,
+      text: textChatbox,
+      "id":,
+    } */
+    let messageForHistorique = data;
+    messageForHistorique.id = socket.id;
+    fs.readFile("./data/historique.json", (err, data) => {
+      console.dir(messageForHistorique);
+      console.dir(JSON.parse(data));
+      let historique = JSON.parse(data).historique;
+      historique.push(messageForHistorique);
+      fs.writeFile(
+        "./data/historique.json",
+        JSON.stringify({ historique: historique }),
+        (err) => {}
+      );
+    });
     socket.broadcast.emit("resmessage", { data: data });
+  });
+  socket.on("disconnect", () => {
+    let deleteIndex;
+    userList.forEach((value, index) => {
+      if (socket.id === value.id) {
+        deleteIndex = index;
+      }
+    });
+    userList.splice(deleteIndex, 1);
+    socket.broadcast.emit("disconnectUser", { userList: userList });
   });
 });
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,59 @@
 const socket = io();
 const chatbox = document.getElementById("chatbox");
 const send = document.getElementById("send");
-
+const messages = document.getElementById("messages");
+const users = document.querySelector("#users ul");
+let pseudo = prompt("Quel est votre super pseudo ?");
+console.log(pseudo);
+let userList = [];
+let id;
+// mette à jour la liste de mes utilisateus dans l'HTML.
+const refreshUser = (list) => {
+  let contenu = "";
+  list.forEach((element) => {
+    contenu += `<li>${element.pseudo}</li>`;
+  });
+  users.innerHTML = contenu;
+};
+socket.emit("newUser", { pseudo: pseudo });
 send.addEventListener("click", () => {
   let textChatbox = chatbox.value;
-  socket.emit("message", { text: textChatbox });
+  let ms = Date.now();
+  socket.emit("message", {
+    pseudo: pseudo,
+    time: ms,
+    text: textChatbox,
+  });
+  console.log(moment(ms).fromNow());
+  messages.innerHTML += `
+  <div>
+    <div>${pseudo}</div>
+    <div>${textChatbox}</div>
+    <div>${moment(ms).fromNow()}</div>
+  </div>`;
+  chatbox.value = "";
 });
-socket.on("resmessage", (data) => {
-  console.dir(data);
+socket.on("resMessage", (reception) => {
+  console.dir(reception);
+  messages.innerHTML += `
+  <div>
+    <div>${reception.data.text}</div>
+    <div>${moment(reception.data.time).fromNow()}</div>
+  </div>`;
+});
+socket.on("resNewUser", (resNewUser) => {
+  userList = resNewUser.userList;
+  console.dir(userList);
+  refreshUser(userList);
+});
+socket.on("welcome", (welcome) => {
+  userList = welcome.userList;
+  userList.push({ pseudo: pseudo, id: welcome.id });
+  console.dir(userList);
+  refreshUser(userList);
+});
+socket.on("disconnectUser", (disconnectUser) => {
+  userList = disconnectUser.userList;
+  console.dir(userList);
+  refreshUser(userList);
 });
